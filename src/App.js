36a1b3e77b09eaa299acacb7a21ae50999f29175
LilShah <34099012+LilShah@users.noreply.github.com>
2021-03-28 19:15:37 +0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "react-headroom";
+import { Link, animateScroll } from "react-scroll";
+import Headroom from "react-headroom";
+import React from "react";
+
+const textStyle = { color: "#353636" };
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Headroom
+          style={{
+            webkitTransition: "all .5s ease-in-out",
+            color: "#FFFFFF",
+            backgroundColor: "#303030",
+            width: "100%",
+          }}
         >
-          Learn React
-        </a>
+          <Link
+            activeClass="active"
+            to="textBox1"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            style={{ padding: "20px" }}
+          >
+            sec1
+          </Link>
+
+          <Link
+            activeClass="active"
+            to="sec2"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            sec2
+          </Link>
+        </Headroom>
+        <div className="textBox1" style={textStyle}>
+          <p>
+            The Pu-Berlin is a consulting company founded in 2008 and based in
+            Berlin Germany. It’s run by the owner himself and his Team. We look
+            after the highest level of trust and confidence in our work as well
+            with our clients.
+          </p>
+          <p>
+            Our goal is to offer our clients the level of security in their
+            financial questions they need. We stand for trust and reliability.
+            All projects are selected and, after personal contacts have been
+            held, started with transparency and trust.
+          </p>
+          <p>
+            It is not the absolute number of projects that is important for us,
+            but the quality we offer to each individual client is decisive for
+            us.
+          </p>
+          <p>
+            Our success is the result of understanding client’s needs, tailoring
+            corporate structures in order to satisfy the needs of our clients
+            using legal entities and good tax planning both in Berlin Germany
+            and abroad.
+          </p>
+        </div>
+        <p className="sec2 " style={textStyle}></p>
       </header>
     </div>
   );
