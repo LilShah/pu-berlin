fe05872d5a97d226bf58b779c192f2e16b9304c6
LilShah <34099012+LilShah@users.noreply.github.com>
2021-05-04 20:35:19 +0500

    restructure to allow different email system

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var nodemailer = require("nodemailer");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = 5000;
+
+app.post("/SendEmail", (req, res) => {
+  var messageJSON = req.body;
+  var text = "";
+  for (const [key, value] of Object.entries(messageJSON)) {
+    text += key + ": " + value + "\n";
+  }
+
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "epicbot923@gmail.com",
+      pass: "Epicbot_123",
+    },
+  });
+
+  const mailOptions = {
+    from: "epicbot923@gmail.com",
+    to: "sv.halilaydin@gmail.com",
+    subject: "Notification",
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.end(info.response);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
